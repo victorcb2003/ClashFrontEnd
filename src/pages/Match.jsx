9d5388f4919d83d1eec8d1fe7cac5812ab61d391
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-02-06 16:05:59 +0100

    modification du match

--- src/pages/Match.jsx
+++ src/pages/Match.jsx
@@ -3,7 +3,6 @@ import matchService from '../services/matchService'
 import equipeService from '../services/equipeService'
 import tournoisService from '../services/tournoisService'
 import butService from '../services/butService'
-import { getUser } from '../services/authService'
 import formaDate from '../utils/formaDate'
 
 import { useState, useEffect } from 'react'
@@ -23,36 +22,51 @@ export default function Match() {
     const [showAddBut, setShowAddBut] = useState(0)
     const [deleteButId, setDeleteButId] = useState(null)
     const [editBut, setEditBut] = useState(null)
+    const [form, setForm] = useState({})
+    const [editLieu,setEditLieu] = useState(null)
+    const [editDate, setEditDate] = useState(null)
 
     const [refresh, setRefresh] = useState(false)
-    const [form, setForm] = useState({})
     const { id: matchId } = useParams()
 
     const [error, setError] = useState(null)
     const [success, setSuccess] = useState(null)
     const [timer, setTimer] = useState(null)
 
-    function addBut(id, n) {
-        setShowAddBut(n)
-        setForm({ Equipe_id: id })
+    async function handleEditLieu(){
+        const response = await matchService.updateMatch({lieu : editLieu, Match_id : matchId})
+
+        setEditLieu(null)
+        setRefresh(!refresh)
     }
 
-    async function handleDelete() {
+    async function handleEditDate(){
+        const date = new Date(editDate)
+        const response = await matchService.updateMatch({date_heure : formaDate(date),Match_id : matchId})
+
+        setEditDate(null)
+        setRefresh(!refresh)
+    }
+
+    function addBut(id, n) {
+        setShowAddBut(n)
+        setForm({ Equipe_id: id, User_id : 0, Type_but : 0, date_heure : "" })
+    }
+
+    async function handleDeleteBut() {
         const reponse = await butService.delete(deleteButId)
 
         setDeleteButId(null)
         setRefresh(!refresh)
     }
 
-    async function handleEdit() {
-
-        console.log(editBut)
+    async function handleEditBut() {
 
         const errors = []
         if (!editBut.User_id) {
             errors.push("Veuillez selectionner un joueur")
         }
-        if (![0,1].includes(editBut.Type_but)) {
+        if (![0, 1].includes(editBut.Type_but)) {
             errors.push("Veuillez selectionner un type de but")
         }
         if (!editBut.date_heure) {
@@ -67,11 +81,7 @@ export default function Match() {
 
         const date_heure = formaDate(date)
 
-
-
-        const response = await butService.update({But_id: editBut.But_id, date_heure: date_heure, User_id: editBut.User_id, Type_But: editBut.Type_But, Match_id: matchId })
-
-        console.log(response)
+        const response = await butService.update({ But_id: editBut.But_id, date_heure: date_heure, User_id: editBut.User_id, Type_But: editBut.Type_But, Match_id: matchId })
 
         setEditBut(null)
 
@@ -86,13 +96,11 @@ export default function Match() {
     function getUserById(id) {
         const joueur = [...equipe1.Joueurs, ...equipe2.Joueurs]
         const joueurr = joueur.filter(j => j.id == id)[0]
-        return `${joueurr.prenom} ${joueurr.nom}`
+        if (joueurr) return `${joueurr.prenom} ${joueurr.nom}`
     }
 
     async function handleSubmitBut(e) {
         e.preventDefault();
-
-        console.log(form)
 
         const errors = []
         if (!form.User_id) {
@@ -112,10 +120,8 @@ export default function Match() {
         const date = new Date(new Date(match.date_heure).getTime() + form.date_heure * 60000) // Une minute en milliseconde
 
         const date_heure = formaDate(date)
-        console.log(date_heure)
-        const response = await butService.create({ date_heure: date_heure, User_id: form.User_id, Type_But: form.Type_But, Match_id: matchId })
 
-        console.log(response)
+        const response = await butService.create({ date_heure: date_heure, User_id: form.User_id, Type_But: form.Type_But, Match_id: matchId })
 
         setShowAddBut(false)
 
@@ -123,28 +129,6 @@ export default function Match() {
 
         setRefresh(!refresh)
         showSuccess(response.data.message)
-    }
-
-    function showError(errors) {
-        if (errors.length == 0) return
-        setTimer(null)
-        setError(errors)
-        setTimer(
-            setTimeout(() => {
-                setError(null)
-            }, 5000)
-        )
-    }
-
-    function showSuccess(sucess) {
-        if (success.length == 0) return
-        setTimer(null)
-        setError(success)
-        setTimer(
-            setTimeout(() => {
-                setError(null)
-            }, 5000)
-        )
     }
 
     // Match
@@ -158,7 +142,7 @@ export default function Match() {
                 console.log(err)
             }
         })()
-    }, [])
+    }, [refresh])
 
 
     // Buts
@@ -168,7 +152,6 @@ export default function Match() {
                 if (equipe2 && equipe1 && equipe1.length != 0 && equipe2.length != 0) {
 
                     const buts = (await butService.getButByMatch(matchId)).buts
-                    console.log(buts)
                     setButs1([])
                     setButs2([])
 
@@ -183,7 +166,7 @@ export default function Match() {
                     buts.forEach(but => {
                         if (equipe1Id.includes(but.User_id)) {
                             but1.push(but)
-                        } else {
+                        } else if (true) {
                             but2.push(but)
                         }
                     });
@@ -220,22 +203,66 @@ export default function Match() {
         })();
     }, [match])
 
-    useEffect(() => {
-        console.log(editBut)
-    }, [editBut])
-
     return (
         <>
             {/* <Header /> */}
             <div className='container mx-auto mt-10'>
-                <h1>Match</h1>
+                <h1 className='text-center'>Match</h1>
                 {match ? (
                     <div>
+
                         {equipe1 && equipe2 && (
-                            <h2>{equipe1.nom} - {equipe2.nom}</h2>
+                            <div>
+                                <h2>{equipe1.nom} - {equipe2.nom}</h2>
+                            </div>
                         )}
-                        <p><strong>Date et Heure:</strong> {new Date(match.date_heure).toLocaleString()}</p>
-                        <p> <strong>Lieu:</strong> {match.lieu}</p>
+                        <p>
+                            <strong>Lieu:</strong>
+                        {editLieu != null ? (
+                            <> 
+                            <input type="text" value={editLieu} onChange={(e)=>{setEditLieu(e.target.value)}} />
+                            <button onClick={()=>{handleEditLieu()}}>
+                                Confirmer
+                            </button>
+                            <button onClick={()=>{setEditLieu(null)}} >
+                                Annuler
+                            </button>
+                            </>
+                        ) : (
+                            <>
+                            {match.lieu}
+                            <button onClick={()=>{setEditLieu(match.lieu)}}>
+                                <FaEdit/>
+                            </button>
+                            </>
+                            
+                        )}
+                        </p>
+                        <p><strong>Date et Heure:</strong>
+                        {editDate != null ? (
+                            <>
+                            <input type="datetime-local" value={editDate} onChange={(e)=>{setEditDate(e.target.value)}} />
+
+                            <button onClick={()=>{handleEditDate()}}>
+                                Confirmer
+                            </button>
+                            <button onClick={()=>{setEditDate(null)}} >
+                                Annuler
+                            </button>
+                            </>
+                        ):(
+                            <>
+                            {new Date(match.date_heure).toLocaleString()}
+                            <button onClick={()=>{setEditDate("")}}>
+                                <FaEdit/>
+                            </button>
+                            </>                        
+                        )}
+                        </p>
+                        
+                        
+
+
                         {tournois != null && (
                             <p><strong>Tournois:</strong> {tournois.nom}</p>
                         )}
@@ -285,7 +312,6 @@ export default function Match() {
                                                     <td>
                                                         <form onSubmit={(e) => handleSubmitBut(e)}>
                                                             <select
-                                                                value={form.User_id}
                                                                 defaultValue={"Selectionner un Joueur"}
                                                                 name="butteur"
                                                                 onChange={e => { setForm({ ...form, User_id: e.target.value }) }}
@@ -295,8 +321,7 @@ export default function Match() {
                                                                 </option>
                                                                 {equipe1.Joueurs.map(joueur =>
                                                                     <option value={joueur.id} key={joueur.id}>
-                                                                        {joueur.nom} - {joueur.prenom}
-                                                                        {/* {joueur.id} */}
+                                                                        {joueur.prenom} - {joueur.nom}
                                                                     </option>
                                                                 )}
                                                             </select>
@@ -402,7 +427,7 @@ export default function Match() {
                                                                 max={0}
                                                                 placeholder='Minute du but ?'
                                                                 value={form.date_heure}
-                                                                onChange={e => setForm({ ...form, date_heure: e.target.value })}
+                                                                onChange={(e) => {setForm({ ...form, date_heure: e.target.value })}}
                                                             />
                                                             <input type="submit" />
                                                         </form>
@@ -459,7 +484,7 @@ export default function Match() {
                         <button onClick={() => { setDeleteButId(null) }}>
                             Annuler
                         </button>
-                        <button onClick={() => { handleDelete() }}>
+                        <button onClick={() => { handleDeleteBut() }}>
                             Supprimer
                         </button>
                     </div>
@@ -501,11 +526,11 @@ export default function Match() {
                                     </>
                                 )}
                             </select>
-                            <select 
-                            name="Type_but" 
-                            defaultValue={editBut.Type_but}
-                            value={editBut.Type_but}
-                            onChange={(e)=>{setEditBut({...editBut, Type_but:e.target.value})}}
+                            <select
+                                name="Type_but"
+                                defaultValue={editBut.Type_but}
+                                value={editBut.Type_but}
+                                onChange={(e) => { setEditBut({ ...editBut, Type_but: e.target.value }) }}
                             >
                                 <option value="0">Normal</option>
                                 <option value="1">Penalty</option>
@@ -514,9 +539,9 @@ export default function Match() {
                                 type="number"
                                 min={0}
                                 max={0}
-                                defaultValue={editBut.date_heure} 
+                                defaultValue={editBut.date_heure}
                                 value={editBut.date_heure}
-                                onChange={(e)=>{setEditBut({...editBut , date_heure: e.target.value})}}
+                                onChange={(e) => { setEditBut({ ...editBut, date_heure: e.target.value }) }}
                             />
                         </form>
                     </div>
@@ -524,7 +549,7 @@ export default function Match() {
                         <button onClick={() => { setEditBut(null) }}>
                             Annuler
                         </button>
-                        <button onClick={() => { handleEdit() }}>
+                        <button onClick={() => { handleEditBut() }}>
                             Confirmer
                         </button>
                     </div>
