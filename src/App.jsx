bd312ff98755cf09b36852cddc8b8dd9830a29cd
Victor Combemorel--Bluntz <victor.combemorel@gmai.com>
2026-02-10 15:23:28 +0100

    Ajout de la page Équipe

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home2 from "./pages/Home2"; // Temporary
 import About from "./pages/Login";
 import Home from "./pages/Home";
 import Tournois from "./pages/Tournois";
+import EquipeDisplay from "./pages/EquipeDisplay";
 import Equipe from "./pages/Equipe";
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home2 />} />
       <Route path="/tournois" element={<Tournois />} />
-      <Route path="/equipe/:id" element={<Equipe/>}/>
+      <Route path="/equipe/:id" element={<EquipeDisplay/>}/>
+      <Route path="/equipe" element={<Equipe/>}/>
     </Routes>
   )
 }
